690735958d4826c93241a5c612ff85ff21664479
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2020-05-07 21:02:41 +0200

    Displaying data

--- frontend-web/src/views/scenario-simulator/components/InsightsLog.tsx
+++ frontend-web/src/views/scenario-simulator/components/InsightsLog.tsx
@@ -19,18 +19,22 @@ class InsightsLog extends React.Component<Props, State> {
     render() {
         let conditionString = (id) => (
             <Query query={GET_ROAD_CONDITION_TYPES}>
-                {({ data, loading, error }) => {
-                    if (loading) return <p>Loading</p>;
-                    if (error) return <p>Error</p>;
-                    for (let type of data.roadConditionTypes) {
-                        if (parseInt(type.id) === id) {
-                            return (
-                                type.name
-                            )
+                {
+                        ({ data, loading, error }) => {
+                            if (loading) return <p>Loading</p>;
+                            if (error) return <p>Error</p>;
+
+                            for (let type of data.roadConditionTypes) {
+                                if (parseInt(type.id) === id) {
+                                    return (
+                                        type.name
+                                    );
+                                }
+                            }
+                            
+                            return id;
                         }
-                    }
-                    return id;
-                }}
+                }
             </Query>
         );
 
@@ -43,17 +47,32 @@ class InsightsLog extends React.Component<Props, State> {
                             <div className="log-info-message">{log.text}</div>
                         </div>
                     ) : (
-                            <div className="log-info-message-list">{
-                                log.simulationSceneEvents.length > 0
-                                    ? log.simulationSceneEvents.map(event => (
-                                        <div key={event.roadSegmentId.toString() + event.roadConditionTypeId.toString()}
-                                            className="log-info-message">
-                                            {conditionString(event.roadConditionTypeId)} with
-										value {_.round(event.value, 2)} on segment: {event.roadSegmentId}
-                                        </div>
-                                    ))
-                                    : <div className="log-info-message">Geen verkeersstatussen gevonden.</div>
-                            }
+                            <div className="log-info-message-list">
+                                {
+                                    // @ts-ignore
+                                    (log.simulationSceneEvents.length > 0) ? (
+                                        log.simulationSceneEvents.map(event => (
+                                            <div key={event.roadSegmentId.toString() + event.roadConditionTypeId.toString()}
+                                                className="log-info-message">
+                                                {conditionString(event.roadConditionTypeId)} with value {_.round(event.value, 2)} 
+                                                on {event.roadSegment.roadSegmentType.name}:
+                                                <ul>
+                                                    <li>{event.roadSegment.name}</li>
+                                                    <li>{event.roadSegment.id}</li>
+                                                    <li>{event.roadSegment.roadSegmentType.id}</li>
+                                                    <li>{event.roadSegment.roadSegmentType.description}</li>
+                                                    <li>{event.roadConditionType.id}</li>
+                                                    <li>{event.roadConditionType.name}</li>
+                                                    <li>{event.roadConditionType.description}</li>
+                                                </ul>
+                                            </div>
+                                        ))
+                                    ) : (
+                                            <div className="log-info-message">
+                                                Geen verkeersstatussen gevonden.
+                                            </div>
+                                        )
+                                }
                             </div>
                         )
                 }
@@ -65,7 +84,6 @@ class InsightsLog extends React.Component<Props, State> {
                 <div className="right-pane-simulator-title">Simulatie log</div>
                 <div>{log}</div>
             </div>
-
         );
     }
 }
