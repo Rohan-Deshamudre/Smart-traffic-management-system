75d6686b8aeca70f7a43836381ccf4ac2542aa7d
Rohan Deshamudre <deshamudreshivadarshanrohan@student.tudelft.nl>
2020-05-07 23:25:48 +0200

    added styling

--- frontend-web/src/views/login/Login.tsx
+++ frontend-web/src/views/login/Login.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { Mutation, ApolloConsumer } from 'react-apollo'
 import gql from "graphql-tag";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 
 const LOGIN = gql`
     mutation PostMutation($username: String!, $password: String!) {
@@ -21,14 +27,13 @@ interface Props {
 
 class Login extends React.Component<Props, State> {
 
-
     constructor(props: Props) {
         super(props);
-
-        this.state = { username: '', password: '' };
+        this.state = {
+            username: '',
+            password: ''
+        }
     }
-
-
 
     render() {
         const { username, password } = this.state;
@@ -38,59 +43,67 @@ class Login extends React.Component<Props, State> {
                 <ApolloConsumer>
                     {client => (
                         <Mutation mutation={LOGIN} >
-
                             {(login) => (
                                 <div>
-
-                                    <p>Username:
-                                <input
-                                            type="text"
-                                            value={username}
-                                            onChange={
-                                                (event) => {
-                                                    // TODO Validate stuff
-                                                    this.setState({ username: event.target.value })
-                                                }
-                                            }
-                                        />
-                                    </p>
-
-                                    <p>Password:
-                                <input
-                                            type="password"
-                                            value={password}
-                                            onChange={
-                                                (event) => {
-                                                    // TODO Validate stuff
-                                                    this.setState({ password: event.target.value })
-                                                }
-                                            }
-                                        />
-                                    </p>
-
-                                    <button
-                                        onClick={
-                                            (event) => {
-                                                event.preventDefault();
-                                                login({
-                                                    variables: {
-                                                        username,
-                                                        password
+                                    <Card style={{width: 400}}>
+                                        <CardHeader title={'Login'} style={{textAlign: 'center',background: '#212121', color: '#fff'}}/>
+                                        <CardContent>
+                                            <div>
+                                                <TextField
+                                                    fullWidth
+                                                    label = "Username"
+                                                    type = "text"
+                                                    value={username}
+                                                    onChange={
+                                                        (event) => {
+                                                            // TODO Validate stuff
+                                                            this.setState({ username: event.target.value })
+                                                        }
                                                     }
-                                                }).then((res) => {
-                                                    if (res.data.tokenAuth.token) {
-                                                        // TODO: go to map
-                                                        window.location.reload()
+                                                />
+                                                <br/>
+                                                <TextField
+                                                    fullWidth
+                                                    label = "Password"
+                                                    type="password"
+                                                    value={password}
+                                                    onChange={
+                                                        (event) => {
+                                                            // TODO Validate stuff
+                                                            this.setState({ password: event.target.value })
+                                                        }
                                                     }
-                                                });
-                                                this.setState({ username: '', password: '' })
-                                            }
-                                        }
-                                        title="login"
-                                    />
+                                                />
+                                            </div>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button
+                                                variant= "contained"
+                                                size= "large"
+                                                color= "primary"
+                                                style={{alignItems: 'center'}}
+                                                onClick={
+                                                    (event) => {
+                                                        event.preventDefault();
+                                                        login({
+                                                            variables: {
+                                                                username,
+                                                                password
+                                                            }
+                                                        }).then((res) => {
+                                                            if (res.data.tokenAuth.token) {
+                                                                // TODO: go to map
+                                                                window.location.reload()
+                                                            }
+                                                        });
+                                                        this.setState({ username: '', password: '' })
+                                                    }
+                                                }
+                                            > Login </Button>
+                                        </CardActions>
+                                    </Card>
                                 </div>
                             )}
-
                         </Mutation>
                     )}
                 </ApolloConsumer>
