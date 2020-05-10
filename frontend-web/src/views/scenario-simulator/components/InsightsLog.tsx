5f2fdfbaf897b10dfd4440f16876de5bd86edcb8
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2020-05-10 13:43:05 +0200

    Refitted the pane and added switch statements to account for all cases

--- frontend-web/src/views/scenario-simulator/components/InsightsLog.tsx
+++ frontend-web/src/views/scenario-simulator/components/InsightsLog.tsx
@@ -16,6 +16,38 @@ class InsightsLog extends React.Component<Props, State> {
         super(props);
     }
 
+    getCondition(type) {
+        switch(type) {
+            case "Broken Car":
+                return "brokencar";
+            case "Accident":
+                return "accident";
+            case "Congestion":
+                return "congestion";
+            case "Event":
+                return "event";
+            default:
+                return "roadwork";
+        }
+    }
+
+    getSegment(type) {
+        switch(type) {
+            case "Bridge":
+                return "bridge";
+            case "Connection":
+                return "connection";
+            case "Motorway":
+                return "motorway";
+            case "Road Crossing":
+                return "roadcrossing";
+            case "Tunnel":
+                return "tunnel";
+            default:
+                return "roadsegment";
+        }
+    }
+
     render() {
         let conditionString = (id) => (
             <Query query={GET_ROAD_CONDITION_TYPES}>
@@ -54,48 +86,50 @@ class InsightsLog extends React.Component<Props, State> {
                                         log.simulationSceneEvents.map(event => (
                                             <section className="stats">
                                                 <div className="box">
-                                                    <div key={event.roadSegmentId.toString() + event.roadConditionTypeId.toString()}
-                                                        className="log-info-message">
-                                                        {event.roadConditionType.description}
-                                                        <p>
-                                                            <a><h3>{conditionString(event.roadConditionType.name)}</h3></a> of level {_.round(event.value, 2)} 
-                                                            on {event.roadSegment.name}.
-                                                            <p>{event.roadSegment.roadSegmentType.description}</p>
-                                                        </p>
-                                                        {
-                                                            (event.roadConditionType.name.toString() == "Congestion") ? (
-                                                                <div>
-                                                                    <img src="../../../assets/tree_icons/road_condition/congestion.svg" width="50" height="50"></img>
-                                                                    <img src="../../../assets/tree_icons/road_segment/roadsegment.svg" width="50" height="50"></img>
-                                                                    <div className="button">
-                                                                        <i className="fa fa-exclamation-triangle">
-                                                                            {event.roadSegment.name}
-                                                                        </i>
-                                                                    </div>
-                                                                    <br/>
-                                                                    <p>This stretch of your route is suffering from congestion which means slow moving traffic,
-                                                                        longer trip time and increased vehicular queuing. All vehicles approaching this road segment
-                                                                        are advised to take an alternate route until the condition improves.
-                                                                    </p>
-                                                                </div>
-                                                            ) : (
-                                                                <div>
-                                                                <img src="../../../assets/tree_icons/road_condition/roadwork.svg" width="50" height="50"></img>
-                                                                <img src="../../../assets/tree_icons/road_segment/roadsegment.svg" width="50" height="50"></img>
-                                                                    <div className="button">
-                                                                        <i className="fa fa-exclamation-triangle">
-                                                                            {event.roadSegment.name}
-                                                                        </i>
-                                                                    </div>
-                                                                    <br/>
-                                                                    <p>This stretch road has been occupied for road repairs and maintenance causing
-                                                                        obstructions in your route. All vehicles approaching this road segment are advised to take
-                                                                        an alternate route as it may be partially or completely closed.
-                                                                    </p>
-                                                                </div>
-                                                            )
-                                                        }
-                                                        
+                                                    <div key={event.roadSegmentId.toString() + event.roadConditionTypeId.toString()} className="log-info-message">
+                                                        <a>
+                                                            <h3>{event.roadConditionType.name}</h3>
+                                                        </a>
+                                                        <div>
+                                                            <img src={
+                                                                "../../../assets/tree_icons/road_condition/" + this.getCondition(event.roadConditionType.name.toString()) + ".svg"
+                                                            } width="50" height="50"></img>
+                                                            <img src={
+                                                                "../../../assets/tree_icons/road_segment/" + this.getSegment(event.roadSegment.roadSegmentType.name.toString()) + ".svg"
+                                                            } width="50" height="50"></img>
+                                                            <div className="button">
+                                                                <i className="fa fa-exclamation-triangle">
+                                                                    {event.roadSegment.name}
+                                                                </i>
+                                                            </div>
+                                                            <br/>
+                                                            {
+                                                                (
+                                                                    () => {
+                                                                        switch(event.roadConditionType.name.toString()) {
+                                                                            case "Broken Car":
+                                                                                return <p></p>
+                                                                            case "Accident":
+                                                                                return <p></p>
+                                                                            case "Congestion":
+                                                                                return <p>
+                                                                                    This stretch of your route is suffering from congestion which means slow moving traffic,
+                                                                                    longer trip time and increased vehicular queuing. All vehicles approaching this road segment
+                                                                                    are advised to take an alternate route until the condition improves.
+                                                                                </p>
+                                                                            case "Event":
+                                                                                return <p></p>
+                                                                            case "Roadwork":
+                                                                                return <p>
+                                                                                    This stretch road has been occupied for road repairs and maintenance causing
+                                                                                    obstructions in your route. All vehicles approaching this road segment are advised to take
+                                                                                    an alternate route as it may be partially or completely closed.
+                                                                                </p>
+                                                                        }
+                                                                    }
+                                                                )()
+                                                            }
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </section>
