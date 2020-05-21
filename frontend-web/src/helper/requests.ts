f07cf326d5790e9f8d24e666b9353b9849d85df8
Rohan Deshamudre <deshamudreshivadarshanrohan@student.tudelft.nl>
2020-05-21 17:38:11 +0200

    got rid of duplicate code

--- frontend-web/src/helper/requests.ts
+++ frontend-web/src/helper/requests.ts
@@ -17,27 +17,10 @@ function getRoute(roadSegment, overview) {
 		}
 		resolve(axios.get(SERVER_ADDRESS + string + '?overview=' + overview + '&alternatives=true' + '&geometries=geojson&access_token=' + process.env.MAPBOX_TOKEN));
 	});
-
-}
-
-
-function getAlternateRoute(coords) {
-	return new Promise((resolve) => {
-		let string = '';
-		for(let i = 0; i < coords.length; i++) {
-			if(i === coords.length - 1) {
-				string += coords[i][0] + ',' + coords[i][1];
-			} else {
-				string += coords[i][0] + ',' + coords[i][1] + ';';
-			}
-		}
-		resolve(axios.get(SERVER_ADDRESS + string + '?alternatives=true' + '&geometries=geojson&access_token=' + process.env.MAPBOX_TOKEN))
-	})
 }
 
 
 export const requests = {
 	getRoute: getRoute,
-	getAlternateRoute: getAlternateRoute,
 	host: SERVER_ADDRESS
 };
