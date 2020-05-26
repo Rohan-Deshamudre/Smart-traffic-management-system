eee90216295de25a628c0c5f9e69897834bbd5bb
Mart van der Lugt <m.j.vanderlugt@student.tudelft.nl>
2020-05-26 15:59:27 +0200

    Add trafficflow measurements to traffic status

--- backend/ndw/datex_ii/endpoints/trafficspeed.py
+++ backend/ndw/datex_ii/endpoints/trafficspeed.py
@@ -9,6 +9,8 @@ from ndw.datex_ii.measurement import Measurement
 from ndw.datex_ii import meta
 from utils import xml_zipper
 
+from ndw.datex_ii.endpoints.message_object import spd_typ, int_typ
+
 end_point = "trafficspeed"
 
 
@@ -22,27 +24,47 @@ def store(road_store):
 
 
 def handle_measurement(root, status):
-    speeds = []
+    values = []
     for measurement in root.findall(meta.MEASUREMENT_PINPOINT):
         site_ref = measurement.find("{%s}measurementSiteReference" % meta.XML_DATEX)
         measurement_site = MeasurementSite.objects.filter(
             name=site_ref.get("id")
         ).first()
+
         loc = get_location(measurement_site)
         indices = get_indices(measurement_site)
 
-        hits = 0
-        site_value = 0
+        spd_hits = 0
+        spd_site_value = 0
+
+        int_hits = 0
+        int_site_value = 0
+
         for measure_value in measurement.findall("{%s}measuredValue" % meta.XML_DATEX):
             if int(measure_value.get("index")) in indices:
                 # Get average traffic speed out of data
-                value = Measurement.handle_measurement_type(measure_value)
-                if value > 0:
-                    site_value += value
-                    hits += 1
+                typ, value = Measurement.handle_measurement_type(measure_value)
+                if typ == spd_typ:
+                    if value > 0:
+                        spd_site_value += value
+                        spd_hits += 1
+                if typ == int_typ:
+                    if value > 0:
+                        int_site_value += value
+                        int_hits += 1
 
-        if hits > 0:
+        if spd_hits > 0:
+            print("sped")
             # take the average speed of average speeds of location
-            speeds.append(create_message_object(loc, int(site_value / hits)))
+            values.append(
+                create_message_object(loc, int(spd_site_value / spd_hits), spd_typ)
+            )
 
-    status[get_road_condition_type_with_name("Congestion").id] = speeds
+        if int_hits > 0:
+            print("int")
+            # take the average intensity
+            values.append(
+                create_message_object(loc, int(int_site_value / int_hits), int_typ)
+            )
+
+    status[get_road_condition_type_with_name("Congestion").id] = values
