1ab3f569210b17e080eb88a40c1c6de8da6f609e
Mart van der Lugt <m.j.vanderlugt@student.tudelft.nl>
2020-05-31 20:28:07 +0200

    Change grammer to accept range values

--- backend/decision_support/road_conditions.py
+++ backend/decision_support/road_conditions.py
@@ -10,9 +10,9 @@ from api.road_segments.methods.getter import (
 from subscription.object.methods import create_road_segments
 from utils.route import match
 from .exceptions import NoMeasurementAvailableException
+from api.road_conditions.exceptions import InvalidConditionException
 from .site_id import fetch_site_id
-from utils.retrieve_data import get_traffic_flow, get_traffic_speed, \
-    get_travel_time
+from utils.retrieve_data import get_traffic_flow, get_traffic_speed, get_travel_time
 
 
 int_typ = "i"
@@ -20,6 +20,18 @@ spd_typ = "s"
 wkd_typ = "w"
 tim_typ = "t"
 trv_typ = "v"
+
+# Grammar:
+# i | < |  400            intensity smaller than 400
+# w | > | 5               weekday greater than
+# s | << | 10 | 20        speed between 10 and 20
+# v | >< | 5 | 10         travel time lower than 5 or higher than 10
+
+# i -> intensity
+# s -> speed
+# w -> weekday
+# t -> time (format: epoch)
+# v -> travel time
 
 
 def get_traffic_flow_from(road_segment: RoadSegment):
@@ -44,51 +56,54 @@ def get_travel_time_from(road_segment: RoadSegment):
     coordinates = []
     for seg in RouteSegment.objects.filter(route=road_segment.route).all():
         segment = fetch_site_id(seg)
-        coordinates.append({
-            'latitude': segment.lat,
-            'longitude': segment.lng
-        })
+        coordinates.append({"latitude": segment.lat, "longitude": segment.lng})
 
     return max(get_travel_time(coordinates)) if len(coordinates) > 0 else 0
 
 
-def is_road_condition_active(road_condition: RoadCondition,
-                             road_segment: RoadSegment):
-    # TODO: replace with multiple db columns?
-    symbol = road_condition.value[0]
-    target = road_condition.value[1:-1]
-    con_type = road_condition.value[-1]
+def is_road_condition_active(road_condition: RoadCondition, road_segment: RoadSegment):
+    # TODO: error checking
+    parts = road_condition.condition.split()
+
+    con_type = parts[0]
+    symbol = parts[1]
+    target1 = parts[2]
+    target2 = None if len(parts) < 4 else parts[3]
+
+    result = None
 
     if con_type == wkd_typ:
-        interp(datetime.datetime.today().weekday(), symbol, target)
+        result = interp(datetime.datetime.today().weekday(), symbol, target1, target2)
     elif con_type == tim_typ:
-        interp(int(time.time()), symbol, target)
+        result = interp(int(time.time()), symbol, target1, target2)
     elif con_type == int_typ:
         traffic_flow = get_traffic_flow_from(road_segment)
-        interp(traffic_flow, symbol, target)
+        result = interp(traffic_flow, symbol, target1, target2)
     elif con_type == spd_typ:
         traffic_speed = get_traffic_speed_from(road_segment)
-        interp(traffic_speed, symbol, target)
-    elif con_type = trv_typ:
+        result = interp(traffic_speed, symbol, target1, target2)
+    elif con_type == trv_typ:
         travel_time = get_travel_time_from(road_segment)
-        interp(travel_time, symbol, target)
-    
-    return interp(closest_measurement[val_key], symbol, target)
+        result = interp(travel_time, symbol, target1, target2)
 
-    # i -> intensity
-    # s -> speed
-    # w -> weekend?
-    # t -> time (format: ?)
-    # v -> travel time
+    if result is None:
+        raise InvalidConditionException(road_condition.condition, road_condition.id)
 
-def interp(value, symbol, target):
+    return result
+
+
+def interp(value, symbol, target1, target2):
     # API may return None in case no data is found
     if value is None:
         raise NoMeasurementAvailableException()
 
-    if symbol == "<":
+    if symbol == "<" and not target2:
         return value < target
-    if symbol == ">":
+    if symbol == ">" and not target2:
         return value > target
+    if symbol == "<<" and target2:
+        return target1 < value and value < target2
+    if symbol == "><" and target2:
+        return target1 > value or value > target2
 
-    raise InvalidValueException(symbol)
+    return None
