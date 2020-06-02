e648873e9db1d32a0436cc8863834b8ac8048e44
Rohan Deshamudre <deshamudreshivadarshanrohan@student.tudelft.nl>
2020-06-02 23:06:20 +0200

    add alternate route toolbox

---
+++ frontend-web/src/views/scenario-designer/toolboxes/AlternateRouteToolbox.tsx
@@ -0,0 +1,110 @@
+import * as React from 'react';
+import LocationSelector from "../../../components/map/LocationSelector";
+import '../styles/toolbox.scss';
+import * as _ from 'lodash';
+
+type Props = {
+    altRoute?: { id: number, lng: number, lat: number }[],
+    handleAltRoute: (newRoute: { id: number, lng: number, lat: number }[]) => void,
+    client?: any,
+    disabled?: boolean
+}
+
+type State = {
+    addNewSegmentToggle: boolean
+}
+
+class AlternateRouteToolbox extends React.Component<Props, State>{
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            addNewSegmentToggle: false
+        };
+
+        this.handleLocation = this.handleLocation.bind(this);
+        this.deleteSegment = this.deleteSegment.bind(this);
+    }
+
+
+    componentDidMount() {
+        let routeToBeDrawn: any = [];
+        if (this.props.client) {
+            routeToBeDrawn = this.props.altRoute.map((routeSegment) => [routeSegment.lng, routeSegment.lat]);
+            this.props.client.writeData({
+                data: {
+                    selectedRoute: routeToBeDrawn
+                }
+            });
+        }
+    }
+
+    handleLocation(newLocation: [number, number]) {
+        let newRoute = [...this.props.altRoute, {
+            ...{lng: newLocation[0], lat: newLocation[1]},
+            id: this.props.altRoute.length
+        }];
+        this.setState({
+            addNewSegmentToggle: false
+        });
+        this.props.handleAltRoute(newRoute);
+
+    }
+
+    deleteSegment(segmentId: number) {
+        let newRoute = this.props.altRoute;
+        newRoute = newRoute.filter((routeSegment) => {
+            return routeSegment.id !== segmentId
+        });
+
+        this.setState({
+            addNewSegmentToggle: false
+        });
+
+        this.props.handleAltRoute(newRoute);
+    }
+
+    componentDidUpdate(prevProps) {
+        let routeToBeDrawn: any = [];
+        if (!_.isEqual(this.props.altRoute, prevProps.altRoute) && this.props.client) {
+            routeToBeDrawn = this.props.altRoute.map((routeSegment) => [routeSegment.lng, routeSegment.lat]);
+            this.props.client.writeData({
+                data: {
+                    selectedRoute: routeToBeDrawn
+                }
+            });
+        }
+    }
+
+    render() {
+        let currentRoutes = this.props.altRoute.map((routeSegment) =>
+            <div className="route-item" key={routeSegment.id}>
+                <div className="route-item-body">
+                    <div className="text">Lng:</div>
+                    <div className="number">{routeSegment.lng}</div>
+                    <div className="text">Lat:</div>
+                    <div className="number">{routeSegment.lat}</div>
+                </div>
+                {!this.props.disabled ? <div onClick={() => this.deleteSegment(routeSegment.id)} className="close-button">x</div> : null }
+            </div>
+        );
+
+        return (
+            <div className="toolbox">
+                {currentRoutes}
+                {!this.props.disabled ? (
+                    this.state.addNewSegmentToggle ? (
+                        <LocationSelector handleLocation={this.handleLocation}/>
+                    ) : (
+                        <div className="add-new">
+                            <div onClick={() => this.setState({addNewSegmentToggle: true})}>
+                                Voeg nieuw segment toe
+                            </div>
+                        </div>
+                    )
+                ) : null}
+            </div>
+        );
+    }
+}
+
+export default AlternateRouteToolbox;
