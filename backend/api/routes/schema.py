41497bef59698490f459d6050e2737c751995697
Mart van der Lugt <m.j.vanderlugt@student.tudelft.nl>
2020-06-05 15:57:11 +0200

    Fix resolve_routes query

--- backend/api/routes/schema.py
+++ backend/api/routes/schema.py
@@ -8,17 +8,27 @@ from utils.auth import has_perms
 class RouteObjectType(DjangoObjectType):
     class Meta:
         model = Route
-        exclude_fields = ('routes', 'road_segment_route',)
+
+    # exclude_fields = (
+    #     "routes",
+    #     "road_segment_route",
+    # )
 
 
 class RouteSegmentObjectType(DjangoObjectType):
     class Meta:
         model = RouteSegment
-        exclude_fields = ('route',)
+
+    # exclude_fields = ("route",)
 
 
 class Query(graphene.ObjectType):
+    routes = graphene.List(RouteObjectType)
 
     def resolve_routes(self, info, **kwargs):
-        has_perms(info, ['routes.view_route'])
+        has_perms(info, ["routes.view_route"])
         return Route.objects.all()
+
+
+class Mutation(graphene.ObjectType):
+    pass
