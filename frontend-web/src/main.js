aaf10e3d7d724273da1f9a32507b3daf29edde59
Mart van der Lugt <m.j.vanderlugt@student.tudelft.nl>
2020-06-12 12:04:23 +0200

    Fix error handler

--- frontend-web/src/main.js
+++ frontend-web/src/main.js
@@ -4,10 +4,10 @@ import App from './App.js';
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
-import { onError } from 'apollo-link-error'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+import { onError } from "apollo-link-error";
 // Imports for local store
 import { resolvers } from './local_store/resolvers';
 import { schema } from './local_store/schema';
@@ -31,15 +31,17 @@ const httpLink = createHttpLink({
     uri: process.env.API_URL,
 });
 
-const errorLink = onError(({ gql, net }) => {
-    if (gql) {
-        gql.forEach(({ message, locations, path }) =>
-            console.log(`[GQL ERROR]: ${message}, ${locations}, ${path}`))
-    }
-    if (net) {
-        console.log(`[Network]: ${net}`)
-    }
-})
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+            console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
+        );
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 let isRefreshing = false;
 
