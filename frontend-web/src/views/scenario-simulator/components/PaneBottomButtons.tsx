5695f2b81804e31bbb29f4152907fdafe7ad6322
Agniv Chatterjee <a.chatterjee-3@student.tudelft.nl>
2020-06-21 18:47:59 +0200

    Added the button for download and used response plan in insights log for content

---
+++ frontend-web/src/views/scenario-simulator/components/PaneBottomButtons.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Button from "react-bootstrap/Button";
+import "../../../components/buttons/styles/paneBottomButtons.scss";
+import {Link} from "react-router-dom";
+import {ApolloConsumer, Query} from 'react-apollo';
+import ExportTree from "./ExportTree";
+import gql from "graphql-tag";
+// @ts-ignore
+import undoIcon from "./../../../assets/undo.svg"
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+
+type Props = {
+    id: string
+}
+
+class PaneBottomButtons extends React.Component<Props, {}> {
+
+	render() {
+		return (
+			<div className="d-flex justify-content-between pane-bottom-buttons">
+				<div>
+					<Query query={gql`{currentTreeId @client}`}>
+						{({data}) => {
+                            console.log(data);
+                            console.log(this.props.id);
+                            
+                            return (
+                                <ExportTree 
+                                    scenarioId={data.currentTreeId} 
+                                    responsePlan={this.props.id} />
+							);
+						}}
+					</Query>
+				</div>
+
+				<div>
+					<Link to="/">
+						<ApolloConsumer>
+							{client => (
+								<OverlayTrigger key='top' overlay={
+									<Tooltip id='tooltip-top'>Terug</Tooltip>
+								}>
+									<Button onClick={() =>
+										client.writeData({
+											data: {
+												currentTreeId: null,
+												curNodeId: -1,
+												curNodeType: null,
+											}
+										})}
+									>
+										<img src={undoIcon} alt="Terug"/>
+									</Button>
+								</OverlayTrigger>
+							)}
+						</ApolloConsumer>
+					</Link>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default PaneBottomButtons;
